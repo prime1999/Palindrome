ad5d28ddd20329c56feeb364948e8723903256da
Yakub Moshood <moshoodolabanji22@gmail.com>
2022-12-18 21:04:10 +0100

    first-commit

---
+++ main.js
@@ -0,0 +1,20 @@
+//Assigning variables
+const form = document.querySelector("form");
+const output = document.querySelector(".output");
+const input = document.querySelector("#input");
+
+function onSubmit(e) {
+  e.preventDefault();
+  let text = input.value;
+  let check = text.trim().replaceAll(" ", "");
+  let val = text.split("").reverse().join("").replaceAll(" ", "");
+
+  if (check === val) {
+    output.innerHTML = `
+    <h6>Yes, <span>'${text}'</span> is a palindrome word</h6>
+    `;
+  }
+  input.value = "";
+}
+
+form.addEventListener("submit", onSubmit);
